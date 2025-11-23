10980d135bfc10080c65f9fdc9bec03958622386
enjie06 <anggie.rahmadina904@gmail.com>
2025-11-24 02:46:28 +0700

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,16 +1,19 @@
 <nav id="navbar">
     <div class="nav-container">
-        <a href="{{ route('home') }}" class="logo">
+        {{-- GANTI route('home') dengan URL langsung --}}
+        <a href="/" class="logo">  {{-- ATAU /home jika mau --}}
             <div class="logo-icon">🏔️</div>
             <span>Wisata Sumut</span>
         </a>
         
         <ul class="flex gap-12 text-sm">
-    <li><a href="/home" class="hover:text-white">Home</a></li>
-    <li><a href="/wisata" class="hover:text-white">Wisata</a></li>
-    <li><a href="/about" class="hover:text-white">About</a></li>
-</ul>
-
+            {{-- GUNAKAN URL langsung, bukan named route --}}
+            <li><a href="/" class="hover:text-white">Home</a></li>  {{-- ATAU /home --}}
+            <li><a href="/wisata" class="hover:text-white">Wisata</a></li>
+            <li><a href="/about" class="hover:text-white">About</a></li>
+            {{-- Optional: Tambahkan link ke semua wisata --}}
+            <li><a href="/wisata/all" class="hover:text-white">Semua Wisata</a></li>
+        </ul>
 
         <div class="nav-right">
             <div class="search-btn">🔍</div>
