a024145eb9ad853a5f2039cc4be12413aca12e9d
enjie06 <anggie.rahmadina904@gmail.com>
2025-11-24 02:46:08 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -166,9 +166,10 @@
                 </ul>
 
                 <div class="w-64">
-            <form action="/search" method="GET" class="relative">
+            {{-- Di welcome.blade.php - cari form yang ada --}}
+            <form action="/wisata/search" method="GET" class="relative">  {{-- GANTI ACTION KE SINI --}}
                 <input type="text" name="q" placeholder="Cari wisata..." 
-                       class="w-full bg-white/10 border border-white/20 rounded-lg px-4 py-2 text-white placeholder-gray-300 focus:outline-none focus:border-blue-400 text-sm pr-10">
+                    class="w-full bg-white/10 border border-white/20 rounded-lg px-4 py-2 text-white placeholder-gray-300 focus:outline-none focus:border-blue-400 text-sm pr-10">
                 <button type="submit" class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-300 hover:text-white transition duration-200">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
