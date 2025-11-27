f0f227117156cc41bf39bb15e49fb9ea76c880c7
nepann <naylavaniar@gmail.com>
2025-11-27 11:19:24 +0700

    Update WisataController.php

--- app/Http/Controllers/WisataController.php
+++ app/Http/Controllers/WisataController.php
@@ -13,6 +13,7 @@ class WisataController extends Controller
         
         $rdfFiles = [
             public_path('data/wisata_alam_1.xml'),
+            public_path('data/wisata_alam_2.xml'),
             public_path('data/wisata_religi.xml'),
             public_path('data/wisata_budaya.xml'),
         ];
@@ -22,20 +23,49 @@ class WisataController extends Controller
             if (file_exists($file)) {
                 try {
                     $data = RDFParser::parse($file);
+                    
+                    // DEBUG: Tampilkan detail per file
+                    \Log::info(basename($file) . ' - Total data: ' . count($data));
+                    
+                    // Hitung data dengan jenisWisata yang valid
+                    $withJenisWisata = array_filter($data, function($item) {
+                        return !empty($item['jenisWisata']);
+                    });
+                    \Log::info(basename($file) . ' - Dengan jenisWisata: ' . count($withJenisWisata));
+                    
+                    // Hitung data tanpa jenisWisata
+                    $withoutJenisWisata = array_filter($data, function($item) {
+                        return empty($item['jenisWisata']);
+                    });
+                    \Log::info(basename($file) . ' - Tanpa jenisWisata: ' . count($withoutJenisWisata));
+                    
+                    // Tampilkan sample data tanpa jenisWisata
+                    if (count($withoutJenisWisata) > 0) {
+                        $sample = reset($withoutJenisWisata);
+                        \Log::warning(basename($file) . ' - Sample tanpa jenisWisata: ' . ($sample['nama'] ?? 'Unknown'));
+                    }
+                    
                     $allWisataData = array_merge($allWisataData, $data);
+                    
                 } catch (\Exception $e) {
-                    // Skip file yang error
+                    \Log::error('Error in ' . basename($file) . ': ' . $e->getMessage());
                     continue;
                 }
+            } else {
+                \Log::warning('File not found: ' . basename($file));
             }
         }
+        
+        // DEBUG: Tampilkan total data sebelum convert
+        \Log::info('TOTAL ALL DATA BEFORE CONVERT: ' . count($allWisataData));
         
         // CONVERT FIELD NAMES untuk kompatibel dengan view
         $allWisataData = $this->convertFieldNames($allWisataData);
         
         // Group by jenisWisata
         $wisataAlam = array_filter($allWisataData, function($item) {
-            return isset($item['jenisWisata']) && $item['jenisWisata'] === 'Alam';
+            $isAlam = isset($item['jenisWisata']) && $item['jenisWisata'] === 'Alam';
+            return $isAlam;
         });
         
         $wisataBudaya = array_filter($allWisataData, function($item) {
@@ -50,6 +80,9 @@ class WisataController extends Controller
         $wisataAlam = array_values($wisataAlam);
         $wisataBudaya = array_values($wisataBudaya);
         $wisataReligi = array_values($wisataReligi);
+
+        // DEBUG: Tampilkan jumlah per kategori
+        \Log::info('FINAL COUNTS - Alam: ' . count($wisataAlam) . ', Budaya: ' . count($wisataBudaya) . ', Religi: ' . count($wisataReligi));
 
         return view('wisata', compact('wisataAlam', 'wisataBudaya', 'wisataReligi'));
     }
