f749f04f0b858e06ffd243576285d22693f60125
nepann <naylavaniar@gmail.com>
2025-12-15 14:23:03 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,19 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\WisataController;
 use App\Http\Controllers\SearchController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
 // Homepage
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 
 // Halaman semua wisata
 Route::get('/wisata', [WisataController::class, 'index'])->name('wisata');
 
-// Search
+// Search dengan FUZZY SEARCH & TYPO CORRECTION
 Route::get('/search', [SearchController::class, 'search'])->name('search');
 
 // Halaman about (static)
@@ -19,7 +25,13 @@ Route::get('/about', function () {
     return view('about');
 });
 
-// TEST ROUTES - Bisa dihapus setelah semua berjalan
+/*
+|--------------------------------------------------------------------------
+| TEST ROUTES - Bisa dihapus setelah semua berjalan
+|--------------------------------------------------------------------------
+*/
+
+// Test koneksi Fuseki
 Route::get('/test-fuseki', function() {
     $service = new \App\Services\FusekiService();
     $result = $service->testConnection();
@@ -27,6 +39,7 @@ Route::get('/test-fuseki', function() {
     return response()->json($result);
 });
 
+// Test data dari Fuseki
 Route::get('/test-data', function() {
     $service = new \App\Services\FusekiService();
     $data = $service->getAllWisata();
@@ -34,6 +47,24 @@ Route::get('/test-data', function() {
     return response()->json([
         'total' => count($data),
         'sample' => $data[0] ?? 'No data'
+    ]);
+});
+
+// Test Fuzzy Search Service
+Route::get('/test-fuzzy', function() {
+    $fuzzyService = new \App\Services\FuzzySearchService();
+    
+    // Test typo correction
+    $tests = [
+        'niass' => $fuzzyService->findClosestMatch('niass'),
+        'meadn' => $fuzzyService->findClosestMatch('meadn'),
+        'pantaii' => $fuzzyService->findClosestMatch('pantaii'),
+        'danaw' => $fuzzyService->findClosestMatch('danaw'),
+    ];
+    
+    return response()->json([
+        'message' => 'Fuzzy Search Test',
+        'tests' => $tests
     ]);
 });
 
